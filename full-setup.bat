132a16879a28eba7029087b12337cd39a236f92f
Solomon Cammack <solomon@solomon.eu.com>
2023-06-29 06:00:22 +0100

    Add extra step to setup script

--- full-setup.bat
+++ full-setup.bat
@@ -1,5 +1,6 @@
 @echo off
 call winget install Git.Git
 call git clone https://github.com/slmnio/companion-module-slmngg
+cd companion-module-slmngg
 call winget install OpenJS.NodeJS.LTS
 call npm install
