e4caac24a3bf5b3ad16ddfc10ae09d8a768e88d9
Solomon Cammack <solomon@solomon.eu.com>
2024-07-24 00:12:14 +0100

    Fix setup script

--- full-setup.bat
+++ full-setup.bat
@@ -1,6 +1,3 @@
 @echo off
-call winget install Git.Git
-call git clone https://github.com/slmnio/companion-module-slmngg
-cd companion-module-slmngg
-call winget install OpenJS.NodeJS.LTS
-call npm install
+call winget install Git.Git OpenJS.NodeJS.LTS --accept-source-agreements --accept-package-agreements
+start cmd "git clone https://github.com/slmnio/companion-module-slmngg && cd companion-module-slmngg && npm install"
